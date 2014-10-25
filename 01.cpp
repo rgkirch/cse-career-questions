14e6863d192ba8baf2d8387dd7d4503e03cb5578
Richie Kirchofer <rgkirch@gmail.com>
2014-10-25 10:40:22 -0400

    01.cpp

---
+++ 01.cpp
@@ -0,0 +1,12 @@
+// Find the most frequent integer in an array
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "input format:\nlength of array\nvalues" << endl;
+    int len = 0;
+    cin >> len;
+
+}
